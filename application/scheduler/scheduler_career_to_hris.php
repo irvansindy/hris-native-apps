520658c46c4e714800cb737417a18d9a1feed430
irvansindy <irvanmuhammad22@gmail.com>
2023-11-28 17:41:12 +0700

    fix scheduler

--- application/scheduler/scheduler_career_to_hris.php
+++ application/scheduler/scheduler_career_to_hris.php
@@ -22,6 +22,12 @@
     $db_name2 = "pralonco_hris_dev";
     $db_port2 = "3306";
 
+    // $db_host2 = "127.0.0.1";
+    // $db_user2 = "root";
+    // $db_password2 = "";
+    // $db_name2 = "hris";
+    // $db_port2 = "3306";
+
     $db_connect2 = mysqli_connect($db_host2, $db_user2, $db_password2, $db_name2);
 
     if(mysqli_connect_errno()) {
@@ -33,20 +39,35 @@
         b.*
         FROM employer_applicant a 
         LEFT JOIN log_scheduler b
-            ON a.id_applicant = b.key
-        AND b.key IS NULL";
+            ON a.id_applicant = b.table_id
+        WHERE b.table_id IS NULL";
+
     $query_fetch_comp_vacancy_from_career = "SELECT 
         a.*,
         b.*
         FROM company_vacancy a 
         LEFT JOIN log_scheduler b
-            ON a.id_vacancy = b.key
-        AND b.key IS NULL";
+            ON a.id_vacancy = b.table_id
+        WHERE b.table_id IS NULL";
 
     $result_emp_applicant = mysqli_fetch_all(mysqli_query($db_connect1, $query_fetch_emp_applicant_from_career), MYSQLI_ASSOC);
     $result_comp_vacancy = mysqli_fetch_all(mysqli_query($db_connect1, $query_fetch_comp_vacancy_from_career), MYSQLI_ASSOC);
 
+    // var_dump($result_emp_applicant[0]["id_applicant"]);
+
     for ($i=0; $i < count($result_emp_applicant); $i++) { 
+        $emp_app_id_applicant = $result_emp_applicant[$i]['id_applicant'];
+        $emp_app_userid = $result_emp_applicant[$i]['userid'];
+        $emp_app_id_vacancy = $result_emp_applicant[$i]['id_vacancy'];
+        $emp_app_applied_at = $result_emp_applicant[$i]['applied_at'];
+        $emp_app_receive_at = $result_emp_applicant[$i]['receive_at'];
+        $emp_app_status = $result_emp_applicant[$i]['status'];
+        $emp_app_created_at = $result_emp_applicant[$i]['created_at'];
+        $emp_app_Updated_at = $result_emp_applicant[$i]['Updated_at'];
+        $emp_app_value = $result_emp_applicant[$i]['value'];
+        $emp_app_process = $result_emp_applicant[$i]['process'];
+        $emp_app_progress = $result_emp_applicant[$i]['progress'];
+
         $query_insert_emp_applicant_to_hris = "INSERT INTO `employer_applicant` (
             `id_applicant`,
             `userid`,
@@ -60,37 +81,53 @@
             `process`,
             `progress`
         ) VALUES (
-            '$result_emp_applicant[$i][`id_vacancy`]',
-            '$result_emp_applicant[$i][`userid`]',
-            '$result_emp_applicant[$i][`id_vacancy`]',
-            '$result_emp_applicant[$i][`applied_at`]',
-            '$result_emp_applicant[$i][`receive_at`]',
-            '$result_emp_applicant[$i][`status`]',
-            '$result_emp_applicant[$i][`created_at`]',
-            '$result_emp_applicant[$i][`Updated_at`]',
-            '$result_emp_applicant[$i][`value`]',
-            '$result_emp_applicant[$i][`process`]',
-            '$result_emp_applicant[$i][`progress`]'
+            '$emp_app_id_applicant',
+            '$emp_app_userid',
+            '$emp_app_id_vacancy',
+            '$emp_app_applied_at',
+            '$emp_app_receive_at',
+            '$emp_app_status',
+            '$emp_app_created_at',
+            '$emp_app_Updated_at',
+            '$emp_app_value',
+            '$emp_app_process',
+            '$emp_app_progress'
         )";
 
         $exe_emp_applicant_to_hris = $db_connect2->query($query_insert_emp_applicant_to_hris);
         // log_scheduler
         $query_log_schedule_emp_applicant = "INSERT INTO `log_scheduler` (
-            `key`,
+            `table_id`,
             `initial_table`,
             `created_date`,
             `status`
         ) VALUES (
-            '$result_emp_applicant[$i][`id_vacancy`]',
+            '$emp_app_id_applicant',
             'employer_applicant',
             '$SFdatetime',
             '1'
         )";
 
-        $exe_log_schedule_emp_applicant = $db_connect2->query($query_log_schedule_emp_applicant);
+        $exe_log_schedule_emp_applicant = $db_connect1->query($query_log_schedule_emp_applicant);
     }
 
     for ($i=0; $i < count($result_comp_vacancy); $i++) { 
+        $comp_vacancy_id = $result_comp_vacancy[$i]["id_vacancy"];
+        $comp_vacancy_name = $result_comp_vacancy[$i]["vacancy_name"];
+        $comp_company_name = $result_comp_vacancy[$i]["company_name"];
+        $comp_banner = $result_comp_vacancy[$i]["banner"];
+        $comp_status = $result_comp_vacancy[$i]["status"];
+        $comp_vacancy_detail = $result_comp_vacancy[$i]["vacancy_detail"];
+        $comp_created_by = $result_comp_vacancy[$i]["created_by"];
+        $comp_created_date = $result_comp_vacancy[$i]["created_date"];
+        $comp_modified_by = $result_comp_vacancy[$i]["modified_by"];
+        $comp_modified_date = $result_comp_vacancy[$i]["modified_date"];
+        $comp_reqDegree = $result_comp_vacancy[$i]["reqDegree"];
+        $comp_reqMajor = $result_comp_vacancy[$i]["reqMajor"];
+        $comp_reqCity = $result_comp_vacancy[$i]["reqCity"];
+        $comp_reqState = $result_comp_vacancy[$i]["reqState"];
+        $comp_salary = $result_comp_vacancy[$i]["salary"];
+
         $query_insert_comp_vacancy_to_hris = "INSERT INTO `company_vacancy` (
             `id_vacancy`,
             `vacancy_name`,
@@ -107,21 +144,43 @@
             `reqCity`,
             `reqState`,
             `salary`
+        ) VALUES (
+            '$comp_vacancy_id',
+            '$comp_vacancy_name',
+            '$comp_company_name',
+            '$comp_banner',
+            '$comp_status',
+            '$comp_vacancy_detail',
+            '$comp_created_by',
+            '$comp_created_date',
+            '$comp_modified_by',
+            '$comp_modified_date',
+            '$comp_reqDegree',
+            '$comp_reqMajor',
+            '$comp_reqCity',
+            '$comp_reqState',
+            '$comp_salary'
         )";
 
         $exe_emp_applicant_to_hris = $db_connect2->query($query_insert_comp_vacancy_to_hris);
 
         $query_log_schedule_com_vacancy = "INSERT INTO `log_scheduler` (
-            `key`,
+            `table_id`,
             `initial_table`,
             `created_date`,
             `status`
         ) VALUES (
-            '$result_emp_applicant[$i][`id_vacancy`]',
-            'employer_applicant',
+            '$comp_vacancy_id',
+            'company_vacancy',
             '$SFdatetime',
             '1'
         )";
 
-        $exe_log_schedule_com_vacancy = $db_connect2->query($query_log_schedule_com_vacancy);
+        $exe_log_schedule_com_vacancy = $db_connect1->query($query_log_schedule_com_vacancy);
+
     }
+    
+    // echo json_encode([
+    //     $result_emp_applicant[0],
+    //     $result_comp_vacancy[0]
+    // ]);
